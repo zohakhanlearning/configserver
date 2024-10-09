d0ade9c53d596579b8c0c232f62ac2dd8decf556
zohakhanlearning <zohakhanlearning@gmail.com>
2024-10-09 10:07:54 -0400

    initial GHActions

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-terraform-state-bucket-update"
+  bucket = "my-bucket-${var.environment}"
 
   tags = {
     Name = "terraform-state-update"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,9 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-2"
+  default     = "us-east-1"
+}
+variable "environment" {
+  description = "The environment to deploy (dev, prod)"
+  type        = string
+  default     = "dev"
 }
