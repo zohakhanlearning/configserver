b154eb55533409873e245b2f4d96c2be35d8f830
zohakhanlearning <zohakhanlearning@gmail.com>
2024-10-09 10:24:26 -0400

    initial GHActions

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 # ec2.tf
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0688ba7eeeeefe3cd" # Example AMI ID, choose one appropriate for your region and needs
+  ami           = "ami-0b0ea68c435eb488d" # Example AMI ID, choose one appropriate for your region and needs
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public[0].id
   associate_public_ip_address = true
