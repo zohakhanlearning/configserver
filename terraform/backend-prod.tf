1c9e2b62cd9d1a4da8d2dc6a941b578ba3a600b6
zohakhanlearning <zohakhanlearning@gmail.com>
2024-10-09 13:58:16 -0400

    initial GHActions

--- terraform/backend-prod.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "zoha-terraform-state-bucket-prod"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
