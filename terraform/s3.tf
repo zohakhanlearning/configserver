b7ab3b784a29d41d32646ce32168e5b4ec3c2ccb
zohakhanlearning <zohakhanlearning@gmail.com>
2024-10-09 15:33:18 -0400

    initial GHActions

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -28,12 +28,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
+
+
 # Block public access to the S3 bucket
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
   bucket = aws_s3_bucket.S3-bucket.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
